e90586d741b51e8d0a8772137fa7ebe743ac8c1e
ranoualachheb <ranoua22@gmail.com>
2021-04-11 02:38:50 -0700

    backend endpoints for getting and adding a book and a bit of frontend styling

--- server/controllers/books.js
+++ server/controllers/books.js
@@ -6,10 +6,21 @@ import books from '../models/books.js';
 const router = express.Router();
 
 export const getBooks = async (req, res) => { 
-    res.send('This Works')
+    try {
+        const booksRes = await books.find();
+        res.status(200).json(booksRes);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
 }
 
 export const createBook = async (req, res)=> {
-    res.send('this works too')
-}
+    const newBook = new books(req.body)
+        try {
+            await newBook.save() 
+            res.status(201).json(newBook) 
+        } catch(error) {
+            res.status(409).json({message: error.message})
+        }
+    }
 
